fef289d3a90b2e7cfec000af9c851b5d8096a833
Mouad Oumous <mouadoumous@gmail.com>
2025-11-10 18:47:09 +0100

    populating image manipulation migration

---
+++ database/migrations/2025_11_10_174023_create_image_manipulations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Album;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImageManipulationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('image_manipulations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('path',2000);
+            $table->string('type',25);
+            $table->string('data');
+            $table->string('output_path',2000);
+            $table->timestamp('created_at');
+            $table->foreignIdFor(User::class,'user_id');
+            $table->foreignIdFor(Album::class,'album_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('image_manipulations');
+    }
+};
