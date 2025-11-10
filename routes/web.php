470f5cc0f0032ecfeabab58cf6f64c165339a825
Mouad Oumous <mouadoumous@gmail.com>
2025-11-10 19:30:14 +0100

    populate web routes with album apis

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::apiResource('album', AlbumController::class);
