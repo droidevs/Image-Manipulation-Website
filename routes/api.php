56dbe49f70336d4afc061221d6d57940a0617992
Mouad Oumous <mouadoumous@gmail.com>
2025-11-14 19:35:43 +0100

    update api

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Example route to return authenticated user (using sanctum, optional)
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::prefix('v1')->group(function () {
+        Route::apiResource("album", AlbumController::class);
+        Route::get('image',[ImageManipulationController::class, 'index']);
+        Route::get('image/{image}',[ImageManipulationController::class, 'show']);
+        Route::get('image/by-album/{album}',[ImageManipulationController::class, 'byAlbum']);
+        Route::post('image/resize',[ImageManipulationController::class, 'resize']);
+        Route::delete('image/{image}',[ImageManipulationController::class, 'destroy']);
+    });
 });
 
 // Example public API route
@@ -26,11 +33,3 @@ Route::get('/ping', function () {
     return response()->json(['message' => 'API is working! ✅']);
 });
 
-Route::prefix('v1')->group(function () {
-    Route::apiResource("album", AlbumController::class);
-    Route::get('image',[ImageManipulationController::class, 'index']);
-    Route::get('image/{image}',[ImageManipulationController::class, 'show']);
-    Route::get('image/by-album/{album}',[ImageManipulationController::class, 'byAlbum']);
-    Route::post('image/resize',[ImageManipulationController::class, 'resize']);
-    Route::delete('image/{image}',[ImageManipulationController::class, 'destroy']);
-});
