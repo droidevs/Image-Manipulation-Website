184a80a13e42a06aabbc3eea5781e5035263c3de
Mouad Oumous <mouadoumous@gmail.com>
2025-11-14 16:34:20 +0100

    update image manipulation

--- app/Models/ImageManipulation.php
+++ app/Models/ImageManipulation.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ImageManipulation extends Model
 {
-    //
+    use HasFactory;
+
+    const TYPE_RESIZE = 'resize';
+
+    const UPDATED_AT = false;
+    protected $fillable = [
+        'name',
+        'path',
+        'type',
+        'data',
+        'output_path',
+        'user_path',
+        'album_id'
+    ];
+
 }
