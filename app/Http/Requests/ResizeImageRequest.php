cd231dad587ae09d05211dfa684bf237cddea3a3
Mouad Oumous <mouadoumous@gmail.com>
2025-11-14 16:31:51 +0100

    update resize image request

---
+++ app/Http/Requests/ResizeImageRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use GuzzleHttp\Psr7\UploadedFile;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResizeImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rutes =  [
+            'image' => ['required'],
+            'w' => ['required', 'regex:^\d+(\.\d+)?%?$/'],
+            'h' => 'regex:^\d+(\.\d+)?%?$/',
+            'album_id' => 'exists:\App\Models\Album,id'
+        ];
+
+        $image = $this->all()['image'] ?? false;
+        if($image && $image instanceof UploadedFile) {
+            $rules['image'][] = 'image';
+        } else {
+            $rules['image'][] = 'url';
+        }
+
+        return $rules;
+    }
+}
