f9feed76106fa53fbcddc0652df6ead51c919d31
Mouad Oumous <mouadoumous@gmail.com>
2025-11-26 10:28:26 +0100

    implement dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,49 @@
 
 namespace App\Http\Controllers;
 
-abstract class DashboardController extends Controller
+use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
+
+
+
+
+class DashboardController extends Controller
 {
-    
+    public function dashboard(Request $request)
+    {
+        $user = $request->user();
+
+        return view('dashboard', [
+            'token' => $user->tokens
+        ]);
+    }
+
+    public function showTokenForm()
+    {
+        return view('token-create');
+    }
+
+    public function createToken(Request $request)
+    {
+        $request->validate([
+            'name' => 'required'
+        ]);
+        $tokenName = $request->post('name');
+
+        $user = $request->post('name');
+        $token = $user->createToken($tokenName);
+
+        return view('token-show', [
+            'tokenName' => $tokenName,
+            'token' => $token->plainTextToken
+        ]);
+    }
+
+    public function deleteToken(PersonalAccessToken $token)
+    {
+        $token->delete();
+
+        return redirect('dashboard');
+    }
+
 }
